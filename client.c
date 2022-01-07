0b4a42e7c1a334fdb5a33d951cd9cc08593de59a
Mohamed Meredith <mmeredit@ga-l5.msk.21-school.ru>
2022-01-07 18:02:24 +0300

    Nicetry

--- client.c
+++ client.c
@@ -15,79 +15,77 @@
 #include <unistd.h>
 #include <signal.h>
 
-
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int                     i;
-    int                     negative;
-    unsigned long long int  num;
+	int						i;
+	int						negative;
+	unsigned long long int	num;
 
-    num = 0;
-    i = 0;
-    negative = 1;
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    if (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            negative = -negative;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        num = num * 10 + (str[i] - '0');
-        i++;
-        if (num >= 9223372036854775807 && negative > 0)
-            return (-1);
-        if (num > 9223372036854775807 && negative < 0)
-            return (0);
-    }
-    return ((int) num * negative);
+	num = 0;
+	i = 0;
+	negative = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative = -negative;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+		if (num >= 9223372036854775807 && negative > 0)
+			return (-1);
+		if (num > 9223372036854775807 && negative < 0)
+			return (0);
+	}
+	return ((int) num * negative);
 }
 
-void checkpid (int sig, siginfo_t *info, void *context)
+void	checkpid(int sig, siginfo_t *info, void *context)
 {
-    (void) *info;
-    (void) context;
-    (void) sig;
+	(void) *info;
+	(void) context;
+	(void) sig;
 }
 
-void    mystr(char c, int pid)
+void	mystr(char c, int pid)
 {
-    int  i;
+	int	i;
 
-    i = 0;
-    while (i < 8)
-    {
-        if (c & (1 << i))
-            kill (pid, SIGUSR1);
-        else
-            kill (pid, SIGUSR2);
-        usleep(770);
-        i++;
-    }
+	i = 0;
+	while (i < 8)
+	{
+		if (c & (1 << i))
+			kill (pid, SIGUSR1);
+		else
+			kill (pid, SIGUSR2);
+		usleep(770);
+		i++;
+	}
 }
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    while (s[i] != '\0')
-        i++;
-    return (i);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
-int main(int pd, char **str)
+int	main(int pd, char **str)
 {
-    int pid;
+	int	pid;
 
-    pid = ft_atoi(str[1]);
-    if (pd != 3)
-        return (1);
-    while (*(str[2]++) != '\0')
-        mystr(*(str[2]), pid);
-    mystr(0 , ft_atoi(str[1]));
-    return (0);
+	pid = ft_atoi(str[1]);
+	if (pd != 3)
+		return (1);
+	while (*(str[2]) != '\0')
+		mystr(*(str[2]++), pid);
+	mystr(0, ft_atoi(str[1]));
+	return (0);
 }
-
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mmeredit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 15:24:16 by mmeredit          #+#    #+#             */
-/*   Updated: 2021/12/25 15:24:58 by mmeredit         ###   ########.fr       */
+/*   Updated: 2022/01/07 16:33:39 by mmeredit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,116 +15,106 @@
 #include <unistd.h>
 #include <signal.h>
 
-
-char    *ft_copy(char *dest, char *src, char c)
+char	*ft_copy(char *dest, char *src, char c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = c;
-    dest[i + 1] = '\0';
-    return (dest);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = c;
+	dest[i + 1] = '\0';
+	return (dest);
 }
 
-char    *ft_realloc(char *str, int flag, char c)
+char	*ft_realloc(char *str, int flag, char c)
 {
-    char    *tmp;
+	char	*tmp;
 
-    tmp = (char *)malloc(flag);
-    if (tmp == NULL)
-        return (NULL);
-    tmp = ft_copy(tmp, str, c);
-    return (tmp);
+	tmp = (char *) malloc(flag);
+	if (tmp == NULL)
+		return (NULL);
+	free(str);
+	tmp = ft_copy(tmp, str, c);
+	return (tmp);
 }
 
-void    ft_realstr(char c, char **str, int flag)
+void	ft_realstr(char c, char **str, int flag)
 {
-    *str = (char *)ft_realloc(*str, flag + 2, c);
-    if (*str == NULL)
-    {
-        free(str);
-        return ;
-    }
-    (*str)[flag] = c;
-    (*str)[flag + 1] = '\0'; 
+	*str = (char *)ft_realloc(*str, flag + 2, c);
+	if (*str == NULL)
+	{
+		free(str);
+		return ;
+	}
 }
 
-void ft_newStr(char c, int flag)
+void	ft_newstr(char c, int flag)
 {
-    static char *str;
-    int j;
+	static char	*str;
+	int			j;
 
-    j = 0;
-    if (flag == 0)
-    { 
-        str = (char *)malloc(sizeof(char) + 1);
-        if (str == NULL)
-            return ;
-        str[0] = c;
-        str[1] = '\0';
-    }
-    else if (flag > 0)
-        ft_realstr(c, &str, flag);
-    else
-    {
-        while(str[j])
-        {
-            write (1, &str[j], 1);
-            j++;
-        } 
-        write (1, "\n", 1);
-        free(str);
-    }
+	j = 0;
+	if (flag == 0)
+	{
+		str = (char *)malloc(sizeof(char) + 1);
+		if (str == NULL)
+			return ;
+		str[0] = c;
+		str[1] = '\0';
+	}
+	else if (flag > 0)
+		ft_realstr(c, &str, flag);
+	else
+	{
+		while (str[j])
+		{
+			write (1, &str[j], 1);
+			j++;
+		}
+		write (1, "\n", 1);
+		free(str);
+	}
 }
 
-static inline void    sig1(int sig, siginfo_t *info, void *context)
+void	sig1(int sig, siginfo_t *info, void *context)
 {
-    (void) context;
-    (void) *info;
-    static char c;
-    static int i;
-    static int flag;
+	static int	i;
+	static char	c;
+	static int	flag;
 
-    if (sig == SIGUSR1)
-        c = (c | (1 << i));
-    i++;
-    if (i == 8)
-    {
-        // i = 0;
-        // if (c != 0)
-        //     write (1, &c, 1);
-        // else
-        //     write (1, &"\n", 1);
-        // c = 0;
-        i = 0;
-        if (c == 0)
-        {
-            ft_newStr(c, -1);
-            flag = 0;
-        }
-        else
-            ft_newStr(c, flag++);
-        c = 0;
-    }
+	(void) context;
+	(void) *info;
+	if (sig == SIGUSR1)
+		c = (c | (1 << i));
+	i++;
+	if (i == 8)
+	{
+		i = 0;
+		if (c == 0)
+		{
+			ft_newstr(c, -1);
+			flag = 0;
+		}
+		else
+			ft_newstr(c, flag++);
+		c = 0;
+	}
 }
 
-int main()
+int	main()
 {
-    struct sigaction one;
+	struct sigaction	one;
 
-    one.sa_sigaction = &sig1;
-    one.sa_flags = SA_SIGINFO;
-
-
-    printf("Server PID: %d\n", getpid());
-    sigaction(SIGUSR1, &one, NULL);
-    sigaction(SIGUSR2, &one, NULL);
-    while(1)
-        pause();
-    return (0);
+	one.sa_sigaction = &sig1;
+	one.sa_flags = SA_SIGINFO;
+	printf("Server PID: %d\n", getpid());
+	sigaction(SIGUSR1, &one, NULL);
+	sigaction(SIGUSR2, &one, NULL);
+	while (1)
+		pause();
+	return (0);
 }
